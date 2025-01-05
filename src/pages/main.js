c5d28643182d6386cc670f6e08cf5019c4e467bb
spandita2 <spanditapanda2@gmail.com>
2025-01-05 18:42:35 +0530

    Add files via upload

---
+++ src/pages/main.js
@@ -0,0 +1,20 @@
+
+export const Main=()=>{  // Landing Page
+    return (
+        <div>
+    <main class="main">
+      <div class="container">
+        <h1>My Wishlist</h1>
+        <br/>
+        <h2>Login to see your Wishlist!!</h2>
+      </div>
+    </main>
+
+    <footer>
+      <div class="container footer">
+        <p>&copy; 2024. All rights reserved.</p>
+      </div>
+    </footer>
+    </div>
+    );
+};
